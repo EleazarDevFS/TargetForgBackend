3dfa7892b81f21c2fa82bb0a91969e38f4702c93
EleazarDev <eleazarjarquincv@gmail.com>
2025-08-14 11:21:55 -0600

    refactor: se actualiza la arquitectura

---
+++ src/main/java/com/tuxitech/mx/targetforg/dto/request/person/GenderRequest.java
@@ -0,0 +1,24 @@
+package com.tuxitech.mx.targetforg.dto.request.person;
+
+import jakarta.validation.constraints.*;
+
+import com.tuxitech.mx.targetforg.dto.response.messages.MessagesResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenderRequest {
+
+    @NotNull(message = MessagesResponse.NOT_NULL_ID_GENDER)
+    private Long idGender;
+
+    @NotNull(message = MessagesResponse.NOT_NULL_GENDER)
+    @NotBlank(message = MessagesResponse.NOT_BLANK_GENDER)
+    private String gender;
+}
---
+++ src/main/java/com/tuxitech/mx/targetforg/dto/request/user/UserRequest.java
@@ -0,0 +1,25 @@
+package com.tuxitech.mx.targetforg.dto.request.user;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+import com.tuxitech.mx.targetforg.dto.request.person.PersonRequest;
+import com.tuxitech.mx.targetforg.dto.response.messages.MessagesResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+
+    @NotNull(message = MessagesResponse.NOT_NULL_USER_ID)
+    private Long id; 
+
+    @Valid
+    @NotNull(message = MessagesResponse.NOT_NULL_PERSON)
+    PersonRequest person;
+}
