6518aa3c7e973bd492e7285fc591f4e733bf97b9
EleazarDev <eleazarjarquincv@gmail.com>
2025-08-14 23:10:10 -0600

    fix: se resuelve constructor vacio

--- src/main/java/com/tuxitech/mx/targetforg/model/user/UserModel.java
+++ src/main/java/com/tuxitech/mx/targetforg/model/user/UserModel.java
@@ -8,11 +8,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
